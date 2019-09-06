6189972b1801ca464d2b24f999fc64b9d7817f1e
Emma Prager <eprager@hawk.iit.edu>
2019-09-06 18:59:28 -0500

    add courses to function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$(function() {
-  $("p").text('test test Rambo');
-});
+window.addEventListener("load", courses);
+
+
+function courses() {
+
+  let code = ["CS-411", "CS-450", "ITM-301", "ITMD-562", "MATH-332"]
+  let title = ["Computer Graphics", "Operating Systems", "Intro OS and Hardware I", "Web Site App Development", "Elementary Linear Algebra"]
+
+  let i = 0;
+  for(i=0; i<code.length; i++) {
+    document.write(code[i].bold() + " ");
+    document.write(title[i] + "\n");
+  }
+  
+}
