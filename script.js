8663d4c070e5c08187121753af1666184ed19c4d
Emma Prager <eprager@hawk.iit.edu>
2019-09-06 19:57:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ window.addEventListener("load", courses);
 
 function courses() {
   
-  let category = ["CS", "CS", "ITM", "ITMD", "MATH"]
-  let number = ["411", "450", "301", "562", "332"]
-  let title = ["Computer Graphics", "Operating Systems", "Intro OS and Hardware I", "Web Site App Development", "Elementary Linear Algebra"]
-
   let i = 0;
   for(i=0; i<category.length; i++) {
     document.write(category[i].bold() + "-".bold() + number[i].bold() + " ");
@@ -13,3 +9,7 @@ function courses() {
   }
   
 }
+
+let category = ["CS", "CS", "ITM", "ITMD", "MATH"];
+let number = ["411", "450", "301", "562", "332"];
+let title = ["Computer Graphics", "Operating Systems", "Intro OS and Hardware I", "Web Site App Development", "Elementary Linear Algebra"];
