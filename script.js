fdbe79f248ae0b33658d29c8f79c1ae9e1fa9ef2
Emma Prager <eprager@hawk.iit.edu>
2019-09-06 19:14:29 -0500

    Formatting

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ window.addEventListener("load", courses);
 
 
 function courses() {
-
-  let code = ["CS-411", "CS-450", "ITM-301", "ITMD-562", "MATH-332"]
+  let category = ["CS", "CS", "ITM", "ITMD", "MATH"]
+  let number = ["411", "450", "301", "562", "332"]
   let title = ["Computer Graphics", "Operating Systems", "Intro OS and Hardware I", "Web Site App Development", "Elementary Linear Algebra"]
 
   let i = 0;
-  for(i=0; i<code.length; i++) {
-    document.write(code[i].bold() + " ");
-    document.write(title[i] + "\n");
+  for(i=0; i<category.length; i++) {
+    document.write(category[i].bold() + "-".bold() + number[i].bold() + " ");
+    document.write(title[i] + "<br/>");
   }
   
 }
